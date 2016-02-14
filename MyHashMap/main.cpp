ac45094a2eae02642d39b2d49081a8f70c229865
Grace Lu Li <lu.li.azure@gmail.com>
2016-02-14 13:16:14 -0800

    Initial Commit

---
+++ MyHashMap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyHashMap
+//
+//  Created by Lu Li on 2/14/16.
+//  Copyright © 2016 Grace. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
